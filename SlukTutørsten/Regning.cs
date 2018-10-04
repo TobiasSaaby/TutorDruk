f4d684247a7bfa51bacb51f77e925d8d9d015f35
Tobias Saaby Steffensen <tobias.saaby@live.com>
2018-10-04 18:34:32 +0200

    Init

---
+++ SlukTutørsten/Regning.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlukTutørsten
+{
+    class Regning
+    {
+        public List<Produkt> ting;
+
+        Regning()
+        {
+            ting = new List<Produkt>();
+        }
+
+        double Udregn()
+        {
+            double total = 0;
+            if (ting.Count() != 0)
+            {
+                foreach (var t in ting)
+                {
+                    total += t.pris;
+                }
+            }
+            return total;
+        }
+
+    }
+}
---
+++ SlukTutørsten/Tutor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlukTutørsten
+{
+    class Tutor
+    {
+        private string navn;
+        private string streetNavn;
+
+        Tutor(string Navn, string StreetNavn)
+        {
+            navn = Navn;
+            streetNavn = StreetNavn;
+        }
+    }
+}
